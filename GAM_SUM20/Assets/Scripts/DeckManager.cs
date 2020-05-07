2a340956d6db9a19f8c6f77aa66300a3302c254a
Markel <martxelo94@gmail.com>
2020-05-07 21:08:09 +0200

    GUI layout setup

---
+++ GAM_SUM20/Assets/Scripts/DeckManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class DeckManager : MonoBehaviour
+{
+
+    public Image[] cards;
+    int selected_card = -1;
+    public float selection_scale = 1.1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void UnselectCard(int idx)
+    {
+        selected_card = -1;
+        cards[idx].transform.scale *= selection_scale;
+    }
+
+    public void SelectCard(int idx)
+    {
+        // unselect previous card
+        if (selected_card > -1) {
+            UnselectCard(selected_card);
+        }
+        selected_card = idx;
+        cards[idx].transform.scale *= selection_scale;
+    }
+}
